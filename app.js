9940d73d867062ce3cafe5d64145e201770a9f34
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-08 16:55:46 +0100

    middleware

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
+const { create } = require("domain");
 
 const app = express();
 
+// 1) MIDDLEWARES
+app.use(morgan("dev"));
+
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.status(200).send("Hello form the server side!");
-// });
-const clinics = JSON.parse(
+app.use((req, res, next) => {
+  console.log("Hello Devs 😊");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTE HANDLERS
+
+const healthcare = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/clinics.json`)
 );
 
 const getAllHealthCares = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: "success",
-    results: clinics.length,
+    requestedAt: req.requestTime,
+    results: healthcare.length,
     data: {
-      clinics,
+      healthcare,
     },
   });
 };
@@ -25,9 +42,9 @@ const getAllHealthCares = (req, res) => {
 const getHealthCare = (req, res) => {
   res.status(200).json({
     status: "success",
-    results: clinics.length,
+    results: healthcare.length,
     data: {
-      clinics,
+      healthcare,
     },
   });
 };
@@ -40,7 +57,7 @@ const createHealthCare = (req, res) => {
 const updateHealthCare = (req, res) => {
   res.status(200).json({
     data: {
-      tour,
+      healthcare,
     },
   });
 };
@@ -55,20 +72,67 @@ const deleteHealthCare = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined",
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined",
+  });
+};
+
+const healthcareRouter = express.Router();
+const userRouter = express.Router();
 // app.get("/api/v1/healthcares", getAllHealthCares);
 // app.get("/api/v1/healthcares/:id", getHealthCare);
 // app.post("/api/v1/healthcares", createHealthCare);
 // app.patch("/api/v1/healthcares/:id", updateHealthCare);
 // app.delete("/api/v1/healthcares/:id", deleteHealthCare);
 
-app.route("/api/v1/healthcares").get(getAllHealthCares).post(createHealthCare);
+// 3) ROUTES
 
-app
-  .route("/api/v1/healthcares/:id")
+healthcareRouter.route("/").get(getAllHealthCares).post(createHealthCare);
+
+healthcareRouter
+  .route("/:id")
   .get(getHealthCare)
   .patch(updateHealthCare)
   .delete(deleteHealthCare);
 
+userRouter.route("/").get(getAllUsers).post(createUser);
+
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/healthcares", healthcareRouter);
+app.use("/api/v1/users", userRouter);
+
+// 4) START SERVER
 const port = 3002;
 app.listen(port, () => {
   console.log(`App running on port ${port}... `);
