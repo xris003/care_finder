add526e2658ac94cceb2ac0efdc3fcc22940caf0
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-16 09:33:19 +0100

    unhandledRejection

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,27 @@
-const AppError = require("../utils/appError");
+const AppError = require("./../utils/appError");
 
 const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}`;
+  const message = `Invalid value for ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
 
+// const handleDuplicateFieldsDB = (err) => {
+//   const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+//   console.log(value);
+//   const message = `Duplicate Field Value: ${value} Please use another value`;
+//   return new AppError(message, 400);
+// };
+
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
-  consoel.log(value);
-  const message = `Duplicate Field Value: x. Please use another value`;
+  const name = err.keyValue.name;
+  const message = `Duplicate Field Value: ${name} Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
 };
 
 const sendErrorDev = (err, res) => {
@@ -42,15 +55,19 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
+  console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
-    if (error.name === "CastError") error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    sendErrorProd(error, res);
+    //let error = { ...err }; --> This destructure
+    if (err.name === "CastError") err = handleCastErrorDB(err);
+    if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+    if (err.name === "ValidationError") err = handleValidationErrorDB(err);
+
+    sendErrorProd(err, res);
   }
 };
--- server.js
+++ server.js
@@ -20,6 +20,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3002;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}... `);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 🚨 Shutting Down....");
+  server.close(() => {
+    process.exit(1);
+  });
 });
