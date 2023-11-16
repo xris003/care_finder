e5636390adaa9357b9d59853713d391935d5efb3
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-16 16:43:41 +0100

    updatedController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,38 @@
+const jwt = require("jsonwebtoken");
 const catchAsync = require("../utils/catchAsync");
 const Admin = require("./../models/adminModel");
+const AppError = require("../utils/appError");
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newAdmin = await Admin.create(req.body);
 
+  const token = jwt.sign({ id: newAdmin._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       admin: newAdmin,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) If email and password exists
+  if (!email || !password) {
+    next(new AppError("Please provide email and password", 400));
+  }
+
+  // 2) if admin and password is correct
+
+  // 3) if ok send token to client
+  const token = "";
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+};
--- models/adminModel.js
+++ models/adminModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const adminSchema = new mongoose.Schema({
   healthEmail: {
@@ -26,6 +27,7 @@ const adminSchema = new mongoose.Schema({
     type: String,
     required: [true, "Enter a password"],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -37,6 +39,14 @@ const adminSchema = new mongoose.Schema({
       message: "Passwords are not the same",
     },
   },
+});
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
 });
 
 const Admin = mongoose.model("Admin", adminSchema);
