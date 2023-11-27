3aae1cbc641e25ee2f3c8103a8dcfbccbc69576e
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-27 12:31:45 +0100

    security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globaErrorHandler = require("./controllers/errorController");
@@ -11,11 +12,15 @@ const adminsRouter = require("./routes/adminsRoutes");
 const app = express();
 
 // 1) MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -24,13 +29,15 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
 
 app.use((req, res, next) => {
   console.log("Hello Devs 😊");
   next();
 });
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
