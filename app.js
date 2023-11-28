69a9eb13f150569a0a7e61bae028e59e0b2fb379
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-28 20:51:03 +0100

    xss

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.json({ limit: "10kb" }));
 app.use(mongoSanitize());
 
 // Data Sanitization against XSS
+app.use(xss());
 
 app.use((req, res, next) => {
   console.log("Hello Devs 😊");
