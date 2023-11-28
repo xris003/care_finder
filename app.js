f17ddfc4dca5255788f71378093d8172a3cf0910
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-28 20:45:58 +0100

    datacleaning

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globaErrorHandler = require("./controllers/errorController");
@@ -31,6 +33,11 @@ app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
+
+// Data Santization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
 
 app.use((req, res, next) => {
   console.log("Hello Devs 😊");
