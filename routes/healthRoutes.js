7961deb44f4d49211d5eb921d39b7ae0f75d0abb
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-11-29 21:58:55 +0100

    logoutup

--- routes/healthRoutes.js
+++ routes/healthRoutes.js
@@ -11,6 +11,7 @@ router.param("id", (req, res, next, val) => {
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 router.patch(
